58387ec5efbe7b21e8647249fe1918022de78891
JackStewart808 <stewa149@go.stockton.edu>
2024-12-10 23:28:41 -0500

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,16 @@
+package stats.pokemon.cards;
+
+public class Card
+{
+    public String name;
+
+    public void setName(String inputName)
+    {
+        name = inputName;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
